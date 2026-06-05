476bd6f21dee3acde0d0a22b685766fd0521d905
Artem Voloschenko <avoloschenko008@gmail.com>
2026-06-05 19:18:18 +0300

    add Permanent class

---
+++ src/main/java/ua/fiders/model/Permanent.java
@@ -0,0 +1,46 @@
+package ua.fiders.model;
+
+import ua.fiders.model.cards.*;
+
+public class Permanent {
+
+    private final Card baseCard;
+    private Player controller;
+    private boolean isTapped;
+
+    private int damageTaken;
+    private int tempDamageBuff, tempHpBuff;
+
+    public Permanent(Card baseCard, Player controller) {
+        this.baseCard = baseCard;
+        this.controller = controller;
+        this.isTapped = true;
+        this.damageTaken = 0;
+        this.tempDamageBuff = 0;
+        this.tempHpBuff = 0;
+    }
+
+    public void tap() {
+        isTapped = true;
+    }
+
+    public void untap() {
+        isTapped = false;
+    }
+
+    public boolean isTapped() {
+        return isTapped;
+    }
+
+    public boolean hasKeyword(CardKeywords keyword){
+        return baseCard.getKeywords().contains(keyword);
+    }
+
+    public Card getBaseCard() {
+        return baseCard;
+    }
+
+    public Player getController() {
+        return controller;
+    }
+}
