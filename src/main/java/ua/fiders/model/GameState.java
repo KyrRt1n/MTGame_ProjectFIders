eb9d203d536809f9148d0953c1c01adaa92177e0
Artem Voloschenko <avoloschenko008@gmail.com>
2026-06-06 13:33:59 +0300

    GameState holds info about game

---
+++ src/main/java/ua/fiders/model/GameState.java
@@ -0,0 +1,43 @@
+package ua.fiders.model;
+
+import java.util.*;
+
+public class GameState {
+
+    private final Player player1;
+    private final Player player2;
+    private Player currentPlayer;
+
+    private final List<Permanent> battlefield;
+
+    public GameState(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        this.currentPlayer = player1;
+        this.battlefield = new ArrayList<>();
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public List<Permanent> getBattlefield() {
+        return battlefield;
+    }
+
+    public void passTurn() {
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
+    }
+}
