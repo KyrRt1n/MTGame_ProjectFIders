db9b3b372a22f378e12caad72dcea4956b4aee97
K1Desktop <avoloschenko008@gmail.com>
2026-06-15 01:54:38 +0300

    Working main

--- src/main/java/ua/fiders/Main.java
+++ src/main/java/ua/fiders/Main.java
@@ -1,9 +1,9 @@
 package ua.fiders;
+
+import ua.fiders.ui.MainApp;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        MainApp.main(args);
     }
 }
